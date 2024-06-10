09b94e2f38d3688ab19a2615a74129d5f160e53d
fuutot <fuutt659@gmail.com>
2024-06-10 21:39:33 +0900

    update データベースと接続

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -3,6 +3,17 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
+    # データベースを初期化
+    from . import db
+    db.init_app(app)
+
+    # appに機能グループの追加
+    from . import auth
+    from . import todo
+    
+    app.register_blueprint(auth.bp)
+    app.register_blueprint(todo.bp)
+    
     @app.route("/hello")
     def hello():
         return "Hello, World!"
