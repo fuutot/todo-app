747575820344eb400223326c1129496caa4cc779
fuutot <fuutt659@gmail.com>
2024-06-27 21:26:34 +0900

    fix test_configを渡した

--- src/app/__init__.py
+++ src/app/__init__.py
@@ -1,11 +1,31 @@
+import os
+
 from flask import Flask
 
-def create_app():
-    app = Flask(__name__)
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True) # todo instance_relative_configとは
+
+    app.config.from_mapping( # 構成を設定
+        SECRET_KEY='dev',
+        DATABASE=os.path.join(app.instance_path, 'app.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True) # config.pyを利用して構成を更新
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
 
     # データベースを初期化
-    from . import db
-    db.init_app(app)
+    from . import model
+    model.init_app(app)
 
     # appに機能グループの追加
     from . import auth
@@ -13,6 +33,7 @@ def create_app():
     
     app.register_blueprint(auth.bp)
     app.register_blueprint(todo.bp)
+    app.add_url_rule('/', endpoint='index')
     
     @app.route("/hello")
     def hello():
