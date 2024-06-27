56b3ffe203e036d47d5dca5a519099606b7fecf3
fuutot <fuutt659@gmail.com>
2024-06-27 21:28:56 +0900

    fix importエラーを解決した

--- src/app/auth.py
+++ src/app/auth.py
@@ -5,7 +5,7 @@ from flask import (
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
-from flaskr.db import get_db
+from app.model import get_db
 
 # auth機能グループの作成
 bp = Blueprint('auth', __name__, url_prefix='/auth')
